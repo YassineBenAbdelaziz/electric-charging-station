561210f15e8b9f0587708b5f9e5aaf4d2321a972
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-03-27 20:47:49 +0100

    Cmake setup

---
+++ build/CMakeFiles/charging_station.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/charging_station.dir/main.cpp.o"
+  "charging_station"
+  "charging_station.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/charging_station.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
