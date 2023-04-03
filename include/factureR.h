4be2fa52ba461b72535825d0da86d0dd510c3540
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-03 23:11:58 +0100

    factureR class header

---
+++ include/factureR.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "factureC.h"
+
+
+using namespace std ;
+
+class sessionReserve ;
+
+class factureR : public factureC
+{
+private:
+    float quantite ;
+    sessionReserve* sess;
+public:
+    factureR();
+    factureR (int num , Date d , bool im , client* cl, sessionReserve* s) ;
+    ~factureR () ;
+
+
+    float calculPrix() ;
+    void setSession(sessionReserve* s) {sess = s ;}
+    sessionReserve* getSession() {return sess ;} 
+
+    friend ostream & operator << (ostream &out, const factureR& f ) ;
+    friend istream& operator>>(istream& in, factureR& f) ;
+};
