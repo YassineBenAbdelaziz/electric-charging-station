4d4371a0af6fdfa4443bd34746ba0503739d18d7
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-03 23:09:58 +0100

    chargeurL  class header

---
+++ include/chargeurL.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "chargeur.h"
+
+
+
+using namespace std ;
+
+
+
+class chargeurL : public chargeur
+{
+protected:
+    int numero_port ;
+    bool status ;
+    float prixKW ;
+    
+public:
+    chargeurL()  ; 
+    chargeurL(chargeur c , int num , bool stat, float prix)  ; 
+    ~chargeurL()  ;
+
+    float getPrixKW() { return prixKW ;}
+    void activerChargeur() {status = true ;}
+    void desactiverChargeur() {status = false ;}
+    int getNum() {return numero_port;} 
+    friend ostream & operator << (ostream &out, const chargeurL&  c) ;
+    friend istream& operator>>(istream& in, chargeurL& c) ;
+    
+};
