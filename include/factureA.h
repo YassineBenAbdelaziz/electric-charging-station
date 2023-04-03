b7c40fa162bf43c470df24008d375112769e4726
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-03 23:11:47 +0100

    factureA class header

---
+++ include/factureA.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "factureC.h"
+#include "chargeurD.h"
+
+
+using namespace std ;
+
+class factureA : public factureC
+{
+private:
+    int quantite ;
+    chargeurD* ch ;
+public:
+    factureA () ;
+    factureA (int num , Date d , bool im  , client* cl,int quantite, chargeurD* ch)  ;
+    ~factureA () ;
+
+
+    float calculPrix() ;
+    void setChargeur(chargeurD* c) { ch = c ;}
+
+    friend ostream & operator << (ostream &out, const factureA& f ) ;
+    friend istream& operator>>(istream& in, factureA& f) ;
+};
