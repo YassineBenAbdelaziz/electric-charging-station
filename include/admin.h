3720c9a8c748c438f2105e3d9a4b9980cefe25b6
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-03 23:08:16 +0100

    admin class header

---
+++ include/admin.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include "session.h"
+
+using namespace std ;
+
+
+class station ;
+
+class admin
+{
+private:
+    int id ;
+    string nom ;
+    string prenom ;
+
+    
+    
+
+public:
+    admin() ;
+    admin(int id , string nom , string prenom) ;
+    ~admin() ;
+
+    void ajouterSession(station& st) ;
+    void deleteSession(station& st) ; 
+
+    void ajouterChargeurD(station& st) ;
+    void deleteChargeurD(station& st) ; 
+
+    void ajouterChargeurL(station& st) ;
+    void deleteChargeurL(station& st) ;
+
+
+    friend ostream & operator << (ostream &out, const admin& a ) ;
+    friend istream& operator>>(istream& cin, admin& a) ;
+
+};
