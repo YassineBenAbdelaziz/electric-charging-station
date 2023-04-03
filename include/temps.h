75393f625a1d98be5bc0e43d069fddd37d62ed29
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-03 23:13:42 +0100

    temps  class header

---
+++ include/temps.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+
+using namespace std ;
+
+class temps
+{
+private:
+    int heures ;
+    int minutes ; 
+    
+public:
+    temps() ;
+    temps(int h , int m ) ;
+    ~temps() ;
+
+    bool operator==(const temps& t) ;
+    friend ostream & operator << (ostream &out, const temps& t) ;
+    friend istream& operator>>(istream& in, temps& t) ;
+};
