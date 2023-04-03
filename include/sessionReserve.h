f986b97df86b9a789f075bff0c504f4cb09ce329
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-03 23:12:56 +0100

    sessionReserve class header

---
+++ include/sessionReserve.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "session.h"
+#include "factureR.h"
+#include "voiture.h"
+
+using namespace std ;
+
+
+class sessionReserve : public session
+{
+private:
+    factureR* facture ;
+    voiture* vehicule ;
+
+
+public:
+    sessionReserve() ;
+    sessionReserve(session s , factureR* fac , voiture* v) ;
+    sessionReserve(const session& s) ;
+    ~sessionReserve() ;
+
+    void setFacture(factureR* r) {facture = r ;}
+    void setVoiture(voiture* v) {vehicule = v ;}
+    voiture* getVoiture() {return vehicule ;}
+
+    friend ostream & operator << (ostream &out, const sessionReserve& s ) ;
+    friend istream& operator>>(istream& in, sessionReserve& s) ;
+    sessionReserve operator = (const session& b) ;
+};
