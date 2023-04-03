b38a7c793bc10ea5f8adb100321d9ae9d2061aa1
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-03 23:09:29 +0100

    chargeurD  class header

---
+++ include/chargeurD.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "chargeur.h"
+
+
+
+using namespace std ;
+
+
+
+class chargeurD : public chargeur
+{
+private:
+    float prix ;
+public:
+    chargeurD()  ;
+    chargeurD(chargeur c , float prix)  ;
+    ~chargeurD()  ;
+
+
+    float getPrix() {return prix ;} 
+
+
+    friend ostream & operator << (ostream &out, const chargeurD&  c) ;
+    friend istream& operator>>(istream& in, chargeurD& c);
+};
