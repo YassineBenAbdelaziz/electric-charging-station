ca364642fe21b15365a0ae606a73fe3e18795234
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-06 07:56:20 +0100

    added missing getter

--- include/admin.h
+++ include/admin.h
@@ -22,15 +22,16 @@ public:
     admin(int id , string nom , string prenom) ;
     ~admin() ;
 
-    void ajouterSession(station& st) ;
-    void deleteSession(station& st) ; 
+    void ajouterSession(station* st) ;
+    void deleteSession(station* st) ; 
 
-    void ajouterChargeurD(station& st) ;
-    void deleteChargeurD(station& st) ; 
+    void ajouterChargeurD(station* st) ;
+    void deleteChargeurD(station* st) ; 
 
-    void ajouterChargeurL(station& st) ;
-    void deleteChargeurL(station& st) ;
+    void ajouterChargeurL(station* st) ;
+    void deleteChargeurL(station* st) ;
 
+    int getId() {return id ;}
 
     friend ostream & operator << (ostream &out, const admin& a ) ;
     friend istream& operator>>(istream& cin, admin& a) ;
