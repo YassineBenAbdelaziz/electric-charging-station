192ffafa0ccd8939d286c869ca55d00bcb3d25b3
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-11 00:29:39 +0100

    Added ./date.cpp implementation

---
+++ src/date.cpp
@@ -0,0 +1,62 @@
+#include "date.h"
+#include <iostream>
+using namespace std ;
+
+Date::Date() {
+
+}
+
+Date::Date(int j , int m , int a ) : jour(j) ,mois(m) , annee(a)
+{
+
+}
+
+
+
+bool Date::operator==(const Date& d) {
+
+    if (annee != d.annee) {
+        return false ;
+    }
+    else {
+        if (mois != d.mois) return false ;
+        else {
+            if (jour != d.jour) return false ;
+            else return true ;
+        }
+    }
+}
+
+
+ostream & operator << (ostream &out, const Date& d)
+{
+    if ( &out == &cout ) {
+        out << "Date : " << d.jour << "/" << d.mois << "/" << d.annee << endl ;
+        
+    }
+    else {
+        out << d.jour << " " << d.mois << " " << d.annee  ;
+    }
+
+    return out;
+}
+
+istream& operator>>(istream& in, Date& d)  {
+if ( &in == &cin ) {
+        cout << "Date : " ;
+        in >> d.jour  >> d.mois >>  d.annee  ;
+        
+    }
+    else {
+        in >> d.jour >> d.mois  >> d.annee  ;
+    }
+
+    return in;    
+}
+
+
+
+
+Date::~Date() {
+    
+}
