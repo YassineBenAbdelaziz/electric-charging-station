4c07a0816e626f925399a433a6203ffe848ffbc2
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-04-11 00:29:39 +0100

    Added ./temps.cpp implementation

---
+++ src/temps.cpp
@@ -0,0 +1,54 @@
+#include "temps.h"
+using namespace std ;
+
+temps::temps () {
+
+}
+
+temps ::  temps(int h, int m) : heures(h) , minutes(m)
+{
+
+}
+
+bool temps::operator==(const temps& t) {
+    if (heures != t.heures) return false ;
+    else {
+        if ( minutes != t.minutes) return false ;
+        else return true ;
+    }
+}
+
+
+
+ostream & operator << (ostream &out, const temps& t)
+{
+    if ( &out == &cout ) {
+        out << t.heures << ":" << t.minutes  ; 
+        
+    }
+    else {
+        out << t.heures << " " << t.minutes ;
+    }
+
+    return out;
+}
+
+
+
+istream& operator>>(istream& in, temps& t)  {
+    if ( &in == &cin ) {
+        in >> t.heures >> t.minutes  ; 
+        
+    }
+    else {
+        in >> t.heures >> t.minutes ;
+    }
+
+    return in;    
+}
+
+
+
+temps::~temps() {
+    
+}
