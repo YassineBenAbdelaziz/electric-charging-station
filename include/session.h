9d4c9992548a2c631ac98621ec166b492c45f9f0
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-05-01 23:22:23 +0100

    added getter

--- include/session.h
+++ include/session.h
@@ -4,37 +4,33 @@
 #include "chargeurL.h"
 #include "date.h"
 
+using namespace std;
 
-using namespace std ;
-
-
-class station ;
+class station;
 
 class session
 {
 protected:
-    int id ;
-    Date date ;
-    temps debut ;
-    temps fin ;
-    string etat ; 
-    chargeurL* port ;
+    int id;
+    Date date;
+    temps debut;
+    temps fin;
+    string etat;
+    chargeurL *port;
 
 public:
     session();
-    session(int id  , Date d , temps deb , temps fin , string etat,chargeurL* p);
-    virtual ~session() ;
+    session(int id, Date d, temps deb, temps fin, string etat, chargeurL *p);
+    virtual ~session();
 
-    chargeurL* getPort() { return port ; }
-    Date getDate() {return date ;}
-    temps getTemps() { return debut ;}
-    int getID() { return id ;}
-    void setEtat(string s) {etat = s ;}
-    void setPort(chargeurL* p) { port = p ;}
+    chargeurL *getPort() { return port; }
+    Date getDate() { return date; }
+    temps getTemps() { return debut; }
+    int getID() { return id; }
+    string getEtat() { return etat; }
+    void setEtat(string s) { etat = s; }
+    void setPort(chargeurL *p) { port = p; }
 
-    friend ostream & operator << (ostream &out, const session& s ) ;
-    friend istream& operator>>(istream& in, session& s) ;
+    friend ostream &operator<<(ostream &out, const session &s);
+    friend istream &operator>>(istream &in, session &s);
 };
-
-
-
