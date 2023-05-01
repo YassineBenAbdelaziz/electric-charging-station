40db3d34a41f462f7c96d857d0375067d74d79d5
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-05-01 23:18:55 +0100

    Add taxes attributes

--- include/factureC.h
+++ include/factureC.h
@@ -1,47 +1,39 @@
 #pragma once
 #include <string>
 #include "date.h"
-using namespace std ;
+#include <map>
+using namespace std;
 
-
-class client ;
+class client;
 
 class factureC
 {
 protected:
-    int numFacture ;
-    Date date ;
-    bool impaye ;
-    float montant ;
-    client* cl ;
+    int numFacture;
+    Date date;
+    bool impaye;
+    float montant;
+    client *cl;
+    map<string, float> taxes;
 
 public:
-    factureC() ;
-    factureC(const factureC& f) ;
-    factureC(int num , Date d , bool im  , client* cl) ;
-    virtual ~factureC() ;
+    factureC();
+    factureC(const factureC &f);
+    factureC(int num, Date d, bool im, client *cl);
+    virtual ~factureC();
 
-    
-    int getNum() { return numFacture ;}
-    float getMontant() {return montant ;}
-    client* getClient() { return cl ;}
+    int getNum() { return numFacture; }
+    float getMontant() { return montant; }
+    client *getClient() { return cl; }
 
-    void setClient(client* cl) { this->cl = cl ;}
+    void setClient(client *cl) { this->cl = cl; }
+    void ajouterTax(string str, float f);
+    bool est_impaye() { return impaye; }
+    void payer() { impaye = false; }
 
-    bool est_impaye() {return impaye ;}
-    void payer() {impaye = false ;}
-    
+    factureC &operator=(const factureC &f);
 
-    void operator=(const factureC& f) ;
-
-    virtual void afficherFacture() ;
-    virtual void appliquerRemise() ;
-    virtual float calculPrix()=0 ;
-
-
-
+    virtual void afficherFacture();
+    virtual void appliquerRemise();
+    virtual float calculPrix() = 0;
 };
-
-
-
-
--- src/factureC.cpp
+++ src/factureC.cpp
@@ -1,49 +1,73 @@
 #include "factureC.h"
 #include "client.h"
 #include <iostream>
+#include <iterator>
 
-using namespace std ;
+using namespace std;
 
-
-factureC::factureC() {
-
+factureC::factureC()
+{
 }
-factureC::factureC(const factureC& f){
-    numFacture = f.numFacture ;
-    date = f.date ;
-    impaye = f.impaye ;
-    montant = f.montant ;
-    cl = new client(*f.cl) ; 
-
+factureC::factureC(const factureC &f)
+{
+    numFacture = f.numFacture;
+    date = f.date;
+    impaye = f.impaye;
+    montant = f.montant;
+    cl = new client(*f.cl);
+    taxes = f.taxes;
 }
-factureC::factureC(int num , Date d , bool im  , client* cl)  : numFacture(num),
-    date(d), impaye(im), cl(cl) 
+factureC::factureC(int num, Date d, bool im, client *cl) : numFacture(num),
+                                                           date(d), impaye(im), cl(cl)
 
 {
-    montant = 0 ;
+    montant = 0;
 }
 
-void factureC::afficherFacture() {
-        cout << "\nNumero : " << numFacture << endl ;
-        cout <<  date  ;
-        cout << "Montant : " << montant << endl;
-        cout << "Impaye : " << impaye << endl ;
-        cout << "Client id : " << cl->getId() ;
+void factureC::afficherFacture()
+{
+    cout << "\nNumero : " << numFacture << endl;
+    cout << date;
+    cout << "Montant : " << montant << endl;
+    cout << "Impaye : " << impaye << endl;
+    cout << "Client id : " << cl->getId();
+    if (taxes.size() > 0)
+    {
+        for (auto it : taxes)
+        {
+            cout << endl
+                 << it.first << " : " << it.second;
+        }
+    }
+    else
+        cout << endl
+             << "Aucun Taxe";
 }
 
-void factureC::appliquerRemise() {
-    montant = montant - 0.1 * montant ;
+void factureC::ajouterTax(string str, float f)
+{
+    taxes[str] = f;
 }
 
-void factureC::operator=(const factureC& f) {
-    numFacture = f.numFacture ;
-    date = f.date ;
-    impaye = f.impaye ;
-    montant = f.montant ;
-    cl = new client(*f.cl) ; 
+void factureC::appliquerRemise()
+{
+    montant = montant - 0.1 * montant;
 }
 
+factureC &factureC::operator=(const factureC &f)
+{
+    if (this != &f)
+    {
+        numFacture = f.numFacture;
+        date = f.date;
+        impaye = f.impaye;
+        montant = f.montant;
+        cl = new client(*f.cl);
+        taxes = f.taxes;
+    }
+    return *this;
+}
 
-factureC::~factureC(){
-    
+factureC::~factureC()
+{
 }
