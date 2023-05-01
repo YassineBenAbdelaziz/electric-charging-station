0b646da1ee91915d25cf5f34c0c7ef275954a3fa
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-05-01 23:15:24 +0100

    add calculMontant method

--- include/factureA.h
+++ include/factureA.h
@@ -2,26 +2,29 @@
 #include "factureC.h"
 #include "chargeurD.h"
 
-
-using namespace std ;
+using namespace std;
 
 class factureA : public factureC
 {
 private:
-    int quantite ;
-    chargeurD* ch ;
+    int quantite;
+    chargeurD *ch;
+
 public:
-    factureA () ;
-    factureA (int num , Date d , bool im  , client* cl,int quantite, chargeurD* ch)  ;
-    ~factureA () ;
+    static int id;
+    factureA();
+    factureA(int num, Date d, bool im, client *cl, int quantite, chargeurD *ch);
+    factureA(const factureA &f);
+    ~factureA();
 
+    float calculPrix();
+    void setChargeur(chargeurD *c) { ch = c; }
 
-    float calculPrix() ;
-    void setChargeur(chargeurD* c) { ch = c ;}
-
-    void afficherFacture() ;
+    void afficherFacture();
     void appliquerRemise();
+    void calculMontant();
 
-    friend ostream & operator << (ostream &out, const factureA& f ) ;
-    friend istream& operator>>(istream& in, factureA& f) ;
+    factureA &operator=(const factureA &f);
+    friend ostream &operator<<(ostream &out, const factureA &f);
+    friend istream &operator>>(istream &in, factureA &f);
 };
