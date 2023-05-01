6695a31c3fc589a94b49b4c98db5e8ce1045e10d
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-05-01 23:27:01 +0100

    format document

--- src/chargeurL.cpp
+++ src/chargeurL.cpp
@@ -1,68 +1,62 @@
 #include "chargeurL.h"
 #include <iostream>
 
-using namespace std ;
+using namespace std;
 
-chargeurL::chargeurL() : chargeur() {
-    
+chargeurL::chargeurL() : chargeur()
+{
 }
 
-
-chargeurL::chargeurL(chargeur c , int num , bool stat, float prix) : chargeur(c) ,
-    numero_port(num), status(stat) , prixKW(prix)
+chargeurL::chargeurL(chargeur c, int num, bool stat, float prix) : chargeur(c),
+                                                                   numero_port(num), status(stat), prixKW(prix)
 {
-    
 }
 
-
-
-
-ostream & operator << (ostream &out, const chargeurL&  c)
+ostream &operator<<(ostream &out, const chargeurL &c)
 {
-    if ( &out == &cout ) {
-        out << "\nId : " << c.id << endl ; 
-        out << "Numero : " << c.numero_port << endl ;
-        out << "Puissance : " << c.puissance << endl ;
+    if (&out == &cout)
+    {
+        out << "\nId : " << c.id << endl;
+        out << "Numero port : " << c.numero_port << endl;
+        out << "Puissance : " << c.puissance << endl;
         out << "Debit : " << c.debit << endl;
-        out << "Prix : "  << c.prixKW << endl ;
-        out << "Status : " << c.status ;
-        
+        out << "Prix : " << c.prixKW << endl;
+        out << "Status : " << c.status;
     }
-    else {
-        out << c.id << " " << c.numero_port << " " << c.puissance << " " << c.debit 
-        << " " << c.prixKW << " " << c.status  ;
+    else
+    {
+        out << c.id << " " << c.numero_port << " " << c.puissance << " " << c.debit
+            << " " << c.prixKW << " " << c.status;
     }
 
     return out;
 }
 
-
-istream& operator>>(istream& in, chargeurL& c) {
-    if ( &in == &cin ) {
-        cout << "\nId : " ;
-        in >> c.id  ;
-        cout << "Puissance : " ;
-        in >> c.puissance  ;
-        cout << "Debit : " ;
-        in >> c.debit ;
-        cout << "Prix : "  ;
-        in >> c.prixKW  ;
-        cout << "Status : " ;
-        in >> c.status ;
-        
+istream &operator>>(istream &in, chargeurL &c)
+{
+    if (&in == &cin)
+    {
+        cout << "\nId : ";
+        in >> c.id;
+        cout << "Numero port : ";
+        in >> c.numero_port;
+        cout << "Puissance : ";
+        in >> c.puissance;
+        cout << "Debit : ";
+        in >> c.debit;
+        cout << "Prix : ";
+        in >> c.prixKW;
+        cout << "Status : ";
+        in >> c.status;
     }
-    else {
-        in >>c.id >> c.numero_port >> c.puissance >>c.debit >>c.prixKW >>c.status  ;
+    else
+    {
+        in >> c.id >> c.numero_port >> c.puissance >> c.debit >> c.prixKW >> c.status;
     }
 
-    return in;    
+    return in;
 }
 
-
-
-
-
-
-chargeurL::~chargeurL() {
-    
+chargeurL::~chargeurL()
+{
 }
